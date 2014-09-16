62490a9c8e02b5b52ab4448a13e7beaa67f6f703
JonghunYu <yuzong@naver.com>
2014-09-16 11:03:38 +0900

    fixed typo

---
+++ app/mailers/spree/shipment_mailer_decorator.rb
@@ -0,0 +1,8 @@
+Spree::ShipmentMailer.class_eval do
+  def shipped_email(shipment, resend = false)
+    @shipment = shipment.respond_to?(:id) ? shipment : Spree::Shipment.find(shipment)
+    subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
+    subject += "#{Spree::Store.current.name} 배송안내드립니다. ##{@shipment.order.number}"
+    mail(to: @shipment.order.email, from: from_address, subject: subject)
+  end
+end

d44deb7e48b559edfbb73bbddce6d4124d722c82
JonghunYu <yuzong@naver.com>
2014-10-07 17:20:30 +0900

    force to reload the instance

--- app/models/spree/shipment_decorator.rb
+++ app/models/spree/shipment_decorator.rb
@@ -9,24 +9,26 @@ Spree::Shipment.class_eval do
 
 
   state_machine   :after_shipped_state,   :initial  => :before_ship do
-    before_transition any => same, :do => :check_ship
+
+    before_transition :from => :before_ship, :do => :check_ship
+
     event :complete_ship do
       transition from: :before_ship, to: :local_delivery
     end
     event :complete_local_delivery do
-      transition from: :local_delivery, to: :local_delivery_complete
+      transition from: [:before_ship, :local_delivery], to: :local_delivery_complete
     end
     event :start_oversea_delivery do
-      transition from: [:local_delivery, :local_delivery_complete], to: :overseas_delivery
+      transition from: [:before_ship, :local_delivery, :local_delivery_complete], to: :overseas_delivery
     end
     event :complete_oversea_delivery do
-      transition from: [:local_delivery, :local_delivery_complete, :overseas_delivery], to: :customs
+      transition from: [:before_ship, :local_delivery, :local_delivery_complete, :overseas_delivery], to: :customs
     end
     event :start_domestic_delivery do
-      transition from: [:local_delivery, :local_delivery_complete, :overseas_delivery, :customs], to: :domestic_delivery
+      transition from: [:before_ship, :local_delivery, :local_delivery_complete, :overseas_delivery, :customs], to: :domestic_delivery
     end
     event :complete_domestic_delivery do
-      transition from: [:local_delivery, :local_delivery_complete, :overseas_delivery, :customs, :domestic_delivery], to: :delivered
+      transition from: [:before_ship, :local_delivery, :local_delivery_complete, :overseas_delivery, :customs, :domestic_delivery], to: :delivered
     end
     event :backward_previous_after_shipped_state do
       transition  :delivered => :domestic_delivery,
@@ -37,16 +39,18 @@ Spree::Shipment.class_eval do
                   :local_delivery => :before_ship
     end
   end   #state_machine
+
   def check_ship
     if self.state != 'shipped'
       if self.state == 'pending'
         self.order.payments.each do |p|
           p.capture! if p.state == 'pending'
-          sleep 5
         end
       end
-      self.ship!
+      self.reload
+      return self.ship!
     end
+    true
   end
 
   def after_ship

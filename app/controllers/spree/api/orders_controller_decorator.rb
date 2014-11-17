f1a59509f16bfe3b04b98029ba36f087428648a0
JonghunYu <yuzong@naver.com>
2014-11-17 20:46:05 +0900

    change api controller

--- app/controllers/spree/api/orders_controller_decorator.rb
+++ app/controllers/spree/api/orders_controller_decorator.rb
@@ -5,17 +5,7 @@ Spree::Api::OrdersController.class_eval do
     unless params[:store] == nil and params[:store_order_id] == nil
       unless @order.shipments == nil
         @order.shipments.each do |shipment|
-          if shipment.store.nil?
-            shipment.store = params[:store]
-          else
-            shipment.store = "#{shipment.store},#{params[:store]}"
-          end
-          if shipment.store_order_id.nil?
-            shipment.store_order_id = params[:store_order_id]
-          else
-            shipment.store_order_id = "#{shipment.store_order_id},#{params[:store_order_id]}"
-          end
-          shipment.save
+          shipment.push_store_order_id params[:store], params[:store_order_id]
         end
       end
     end
